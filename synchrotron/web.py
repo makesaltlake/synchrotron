d3aa55c74af70781d6d66a9728975ad36279bf02
Alex Boyd <alex@opengroove.org>
2016-10-30 04:20:14 -0600

    add /trigger

--- synchrotron/web.py
+++ synchrotron/web.py
@@ -1,9 +1,24 @@
 
 import flask
+import redis
+import os
 
 app = flask.Flask('synchrotron')
+
 
 @app.route('/')
 def index():
   return 'ohai'
 
+
+@app.route('/trigger', methods=['POST'])
+def trigger():
+  token = request.form['token']
+  required_token = os.getenv('TRIGGER_TOKEN')
+  if required_token is None or token != required_token:
+    abort(403)
+
+  # TODO: disconnect eagerly
+  r = redis.Redis.from_url(os.getenv('REDIS_URL'))
+  r.publish('trigger', 'go')
+  return 'successful.'
