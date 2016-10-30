a59db395f157e121ce5b93c7695a826a5893f9d0
Alex Boyd <alex@opengroove.org>
2016-10-30 05:50:53 -0600

    send a message to slack on trigger

--- synchrotron/work.py
+++ synchrotron/work.py
@@ -2,31 +2,50 @@
 import redis
 import os
 import time
+import slackclient
 
 
 class SynchrotronWorker:
   def __init__(self):
-    pass
+    self.slack = slackclient.SlackClient(os.getenv('SLACK_TOKEN'))
+    self.slack_channel = os.getenv('SLACK_CHANNEL')
 
   def run(self):
     r = redis.Redis.from_url(os.getenv('REDIS_URL'))
 
     p = r.pubsub(ignore_subscribe_messages=True)
-    p.subscribe('trigger')
+    p.subscribe('trigger', 'sync')
 
     # fire off initial sync
     self.sync()
 
-    # then listen for requests to sync again
+    # then listen for requests
     for message in p.listen():
-      # drain additional messages in case we've been asked to sync multiple times
-      while p.get_message():
-        pass
-      # then sync
-      self.sync()
+      if p['channel'] == 'sync':
+        # TODO: ignore multiple sync messages all sent in short order
+        self.sync()
+      elif p['channel'] == 'trigger':
+        self.trigger(p['data'])
 
   def sync(self):
     print('sync at %s' % time.ctime())
+
+  def trigger(self, address):
+    self.send_slack_message(':wave:')
+
+  def send_slack_message(self, message):
+    self.slack.api_call('chat.postMessage', channel=self.slack_channel, text=message, as_user=True)
+
+
+def retry(function, retries=3):
+  for i in range(retries):
+    try:
+      result = function()
+    except Exception:
+      if i == retries - 1:
+        raise
+    else:
+      return result
 
 
 if __name__ == '__main__':
