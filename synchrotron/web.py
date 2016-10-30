623544b161430a451825d4426e0191420c96589e
Alex Boyd <alex@opengroove.org>
2016-10-30 05:17:13 -0600

    parse the email address out of the signup email body

--- synchrotron/web.py
+++ synchrotron/web.py
@@ -3,6 +3,7 @@ import flask
 from flask import request
 import redis
 import os
+import re
 
 app = flask.Flask('synchrotron')
 
@@ -14,12 +15,32 @@ def index():
 
 @app.route('/trigger', methods=['POST'])
 def trigger():
-  token = request.form.get('token') or request.args.get('token')
+  params = {**request.args, **request.form}
+
+  token = params['token']
   required_token = os.getenv('TRIGGER_TOKEN')
   if required_token is None or token != required_token:
     abort(403)
 
+  address = parse_email(params['body'])
+  if address is None:
+    print('no email address extracted')
+    return 'no email address extracted.'
+
   # TODO: disconnect eagerly
   r = redis.Redis.from_url(os.getenv('REDIS_URL'))
-  r.publish('trigger', 'go')
+  r.publish('trigger', address)
+
+  print('triggered %s' % address)
   return 'successful.'
+
+
+def parse_email(body):
+  if 'subscription was just created on' not in body:
+    return
+
+  match = re.search('Email:\s*([^\s]+)', body)
+  if match is None:
+    return
+
+  return match.group(1)
