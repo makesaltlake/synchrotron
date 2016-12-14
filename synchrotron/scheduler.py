8de05020d2940b9702c9d4edb4a8c9df9f26da7f
Alex Boyd <alex@opengroove.org>
2016-12-14 01:06:59 -0700

    include a dummy message

--- synchrotron/scheduler.py
+++ synchrotron/scheduler.py
@@ -7,7 +7,7 @@ scheduler = BlockingScheduler()
 @scheduler.scheduled_job('cron', hour=3, timezone='America/Denver')
 def trigger_report():
     r = redis.Redis.from_url(os.getenv('REDIS_URL'), charset='utf-8', decode_responses=True)
-    r.publish('report')
+    r.publish('report', '')
 
 
 if __name__ == '__main__':
