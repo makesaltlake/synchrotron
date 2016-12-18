d76daa29c99771c5ce7e71624edd056fa04d6db7
Alex Boyd <alex@opengroove.org>
2016-12-18 05:58:28 -0700

    emoji.

--- synchrotron/work.py
+++ synchrotron/work.py
@@ -142,7 +142,7 @@ class SynchrotronWorker:
   @stripe_event_processor('invoice.payment_failed')
   def process_invoice_payment_failed(self, event):
     self.send_slack_message(
-      text=":alert: %s's payment failed" % self.summarize_customer(event['data']['object']['customer'])
+      text="%s's payment failed :alert:" % self.summarize_customer(event['data']['object']['customer'])
     )
 
   @stripe_event_processor('charge.dispute.created')
